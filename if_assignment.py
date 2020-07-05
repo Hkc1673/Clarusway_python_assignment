43bdabdbb1897124f995c6bbcc65157717c282e2
Hkc1673 <habibkc71@gmail.com>
2020-07-05 21:05:36 +0300

    # if assignment 1

---
+++ if_assignment.py
@@ -0,0 +1,8 @@
+name=input("write your name: ").title()
+name1=("Hello,{}! The password is :W@12".format(name))
+name2=("Hello,{}! See you later.".format(name))
+if name=="Joseph":
+    print(name1)
+else:
+    print(name2)
+
