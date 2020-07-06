ee99016fc97e3faa19b83a22136a14a845c1b9eb
Hkc1673 <habibkc71@gmail.com>
2020-07-06 23:43:48 +0300

    covid19 assignment

---
+++ Covid19.py
@@ -0,0 +1,13 @@
+age=(input("Are you a cigarette addict older than 75 years old? True/False: ").title())== "Yes"
+
+chronic=(input("Do you have a severe chronic disease? True/False: ").title())== "Yes"
+
+immune=(input("Is your immune system too weak? True/False: ").title())=="Yes"
+
+if age or chronic or immune:
+
+    print("risk")
+
+else:
+
+    print("no risk")
